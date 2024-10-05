8e97fb976797e6888c9bdadd9b7c80d08ce77b15
dess1810 <donwarikobv@gmail.com>
2024-10-05 17:21:57 -0600

    react : Card

---
+++ src/Inicio/Acerca.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Card, Row, Col } from 'react-bootstrap'; 
+import Islas from '../img/Islas.jpeg'; 
+import Choby from '../img/Choby.jpeg'; 
+import Jose from '../img/Jose.jpeg';
+import './Acerca.css'
+
+const registros = [
+  {
+    id: 1,
+    name: 'Islas Cabrera Erick Uriel',
+    title: 'Lider',
+    img: Islas,
+  },
+  {
+    id: 2,
+    name: 'Guzman Sanchez Luiz Enrique',
+    title: 'Analista en Base de datos',
+    img: 'https://via.placeholder.com/150', 
+  },
+  {
+    id: 3,
+    name: 'Carvente Miguel Isai',
+    title: 'Desarrollador',
+    img: Choby,
+  },
+  {
+    id: 4,
+    name: 'Martínez Ramírez Dessire Sheresada',
+    title: 'Desarrollador',
+    img: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 5,
+    name: 'Castañeda Castillo Jose Manuel',
+    title: 'Tester',
+    img: Jose,
+  },
+  {
+    id: 6,
+    name: 'Lopez Alcantara Yaneth Berenice',
+    title: 'Tester',
+    img: 'https://via.placeholder.com/150',
+  },
+];
+
+function Otros() {
+  return (
+    <div className="d-flex justify-content-center flex-wrap">
+      {registros.map((registro) => (
+        <Card key={registro.id} className="m-3 custom-card" style={{ width: '10rem', position: 'relative' }}>
+          <Card.Img variant="top" src={registro.img} className="img" />
+          <div className="details">
+            <Card.Body>
+              <Card.Title>{registro.name}</Card.Title>
+              <Card.Text>
+                {registro.title}
+              </Card.Text>
+            </Card.Body>
+          </div>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default Otros;
