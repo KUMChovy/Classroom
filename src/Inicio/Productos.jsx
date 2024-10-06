c5829ca5f76a441320ff53b8236a92798a111be4
KUMChovy <isai.c.miguel.twoinky@gmail.com>
2024-10-05 20:23:23 -0600

    react: productos

--- src/Inicio/Productos.jsx
+++ src/Inicio/Productos.jsx
@@ -3,7 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import './busqueda.css';
+import './productos.css';
 
 function Busqueda() {
     const [showSearch, setShowSearch] = useState(false);
