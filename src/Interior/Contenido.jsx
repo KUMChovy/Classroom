c8367ebc3573012789c70327c9b2e79d8355a8d6
dess1810 <donwarikobv@gmail.com>
2024-10-06 18:05:39 -0600

    react: nav inicio

---
+++ src/Interior/Contenido.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavIni from '../Componentes/NavIni'
+
+function Contenido() {
+  return (
+
+      <NavIni />
+
+  );
+}
+
+export default Contenido;
